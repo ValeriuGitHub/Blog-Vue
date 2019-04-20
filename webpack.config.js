7400d0a54d474028df653c7b97053988b1f37970
Bodnarchuk Valerii <bodnarchukvalole@gmail.com>
2019-04-20 18:05:32 +0300

    Change word, convert indentations on tabs

--- webpack.config.js
+++ webpack.config.js
@@ -2,84 +2,84 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue',
-        options: {
-				  loaders: {
-				    scss: 'style!css!sass'
-				  },
-        }
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(png|svg|jpg|gif|jpeg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'img'
-            }
-          },
-          {
-            loader: 'image-webpack-loader',
-          }
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'fonts'
-          }
-        },
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue'
-    }
-  },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true,
-    port: 5000
-  },
-  devtool: '#eval-source-map'
+	entry: './src/main.js',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		publicPath: '/dist/',
+		filename: 'build.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue',
+				options: {
+					loaders: {
+						scss: 'style!css!sass'
+					},
+				}
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.(png|svg|jpg|gif|jpeg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'img'
+						}
+					},
+					{
+						loader: 'image-webpack-loader',
+					}
+				],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'fonts'
+					}
+				},
+			}
+		]
+	},
+	resolve: {
+		alias: {
+			'vue$': 'vue/dist/vue'
+		}
+	},
+	devServer: {
+		historyApiFallback: true,
+		noInfo: true,
+		port: 5000
+	},
+	devtool: '#eval-source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
+	module.exports.devtool = '#source-map'
+	// http://vue-loader.vuejs.org/en/workflow/production.html
+	module.exports.plugins = (module.exports.plugins || []).concat([
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"production"'
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			}
+		}),
+		new webpack.LoaderOptionsPlugin({
+			minimize: true
+		})
+	])
 }
